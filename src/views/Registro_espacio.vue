119197012afada498f77bec45c7b25afb325c082
SebasDiez84 <sebasdiez84@gmail.com>
2021-03-09 20:51:25 -0500

    Registro de espacios de trabajo Inicial

--- src/views/Registro_espacio.vue
+++ src/views/Registro_espacio.vue
@@ -1,38 +1,58 @@
 <template>
-  <div class="Registro espacio de trabajo">
+  <div id="espacio_trabajo" class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="Registro espacio de trabajo" />
-    
+    <HelloWorld msg="Registro de Espacios de trabajo" />
+   
     <div class="container">
       <section class="form">
         <form>
           <div class="form-row">
-            <div class="col">
-              <input type="text" class="form-control" placeholder="Nombre espacio de proyecto" />
-            </div>
+              <label for="inputNombreEspacioTrabajo">Espacio de trabajo</label>
+              <input
+                v-model="nombre"
+                type="text"
+                class="form-control"
+                id="inputNombreEspacioTrabajo"
+                placeholder="Nombre espacio de trabajo"
+              /> 
           </div>
           <br>
           <div class="form-row">
-            <div class="col">
-              <input type="text" class="form-control" placeholder="Descripción del espacio" />
-            </div>
+            <label for="inputDescripcionEspacio">Descripcion</label>
+            <input
+              v-model="cedula"
+              type="text"
+              class="form-control"
+              id="inputDescripcionEspacio"
+              placeholder="Cedula"
+            />
+            
           </div>
+          <button type="submit" @click.prevent="crearEspacioDeTrabajo" class="btn btn-primary">Registrar</button>
         </form>
       </section>
 
       <section class="data">
-        <caption>
+        
+        <caption >
           Espacios de trabajo
+        </caption>
           <table class="table">
             <thead>
               <tr>
-                <th>Codigo</th>
+                <th>Cedula</th>
                 <th>Nombre</th>
-                <th>Descripcion</th>
+                <th>Correo</th>
               </tr>
             </thead>
+            <tbody>
+              <tr v-for="(investigador) in investigadores" :key="investigador.cedula">
+                <td>{{investigador.cedula}}</td>
+                <td> {{investigador.nombre}} </td>
+                <td> {{investigador.correo}} </td>
+              </tr>
+            </tbody>
           </table>
-        </caption>
       </section>
     </div>
   </div>
@@ -47,5 +67,25 @@ export default {
   components: {
     HelloWorld,
   },
+  data: () => ({
+    cedula: '',
+    nombre: '',
+    apellido: '',
+    correo: '',
+    contraseña: '',
+    investigadores: []
+  }),
+  methods: {
+    crearInvestigador: function() {
+      this.investigadores.push({
+        cedula: this.cedula,
+        nombre: this.nombre,
+        apellido: this.apellido,
+        correo: this.correo,
+        contraseña: this.contraseña,
+      });
+      console.log(this.investigadores)
+    }
+  }
 };
 </script>
