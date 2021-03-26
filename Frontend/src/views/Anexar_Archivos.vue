0f4d5b4813571a867cef6fbf4ae78af8439a713c
Leon-Avalos <heiko.avalos@gmail.com>
2021-03-25 20:53:58 -0500

    chingue su mdre

--- Frontend/src/views/Anexar_Archivos.vue
+++ Frontend/src/views/Anexar_Archivos.vue
@@ -1,37 +1,50 @@
 <template>
-    <div class="container">
-        <h3>Bienvenido al espacio de trabajo: Espacio Test</h3>
-        <form>
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label" for="cedula">Cedula</label>
-                <div class="col-sm-10">
-                    <input class="form-control" type="text" name="cedula" id="cedula">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label" for="nombre">Nombre</label>
-                <div class="col-sm-10">
-                    <input class="form-control" type="text" name="nombre" id="nombre">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="descripcion">Descripción</label>
-                <textarea class="form-control" name="descripcion" id="descripcion" cols="30" rows="6"></textarea>
-            </div>
-
-        </form>
-    </div>
+  <div class="container">
+    <h3>Bienvenido al espacio de trabajo: Espacio Test</h3>
+    <form>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="cedula">Cedula</label>
+        <div class="col-sm-10">
+          <input class="form-control" type="text" name="cedula" id="cedula" />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="nombre">Nombre</label>
+        <div class="col-sm-10">
+          <input class="form-control" type="text" name="nombre" id="nombre" />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="descripcion">Descripción</label>
+        <textarea
+          class="form-control"
+          name="descripcion"
+          id="descripcion"
+          cols="30"
+          rows="6"
+        ></textarea>
+      </div>
+      <div class="input-group mb-3">
+        <div class="custom-file">
+          <input type="file" class="custom-file-input" id="inputGroupFile02" />
+          <label class="custom-file-label" for="inputGroupFile02"
+            >Elegir Archivo</label
+          >
+        </div>
+        <div class="input-group-append">
+          <span class="input-group-text" id="">Subir</span>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Anexar_Archivos",
-    data: () => ({
-
-    })
-}
+  name: "Anexar_Archivos",
+  data: () => ({}),
+};
 </script>
 <style>
-
 </style>
 
