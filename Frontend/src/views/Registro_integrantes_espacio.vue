417251181475b2de3f8287081faf21734788c925
JoanCadena <inmortal_20@live.com>
2021-04-11 15:59:01 -0500

    corregido

--- Frontend/src/views/Registro_integrantes_espacio.vue
+++ Frontend/src/views/Registro_integrantes_espacio.vue
@@ -2,72 +2,133 @@
   <div id="integrantes_espacio_trabajo" class="home">
     <!-- <img alt="Vue logo" src="../static/logo.png"> -->
     <HelloWorld msg="Registro de integrantes en el espacio" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    
-    <br>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
+      crossorigin="anonymous"
+    />
+
+    <br />
 
     <div class="container">
       <form class="was-validated">
         <div class="mb-3">
-          <label for="inputIdEspacioTrabajo" class="form-label">Espacio de trabajo</label>
-          <select v-model="idEspacioDeTrabajo" class="form-select"  aria-label="selectespacio" id="selectIdEspacioTrabajo" required>
+          <label for="inputIdEspacioTrabajo" class="form-label"
+            >Espacio de trabajo</label
+          >
+          <select
+            v-model="idEspacioDeTrabajo"
+            class="form-select"
+            aria-label="selectespacio"
+            id="selectIdEspacioTrabajo"
+            required
+          >
             <option value="">Selecciona un espacio de trabajo</option>
-
           </select>
           <div class="invalid-feedback">Selecciona un espacio de trabajo</div>
         </div>
-        <br>
-        <div class="form-row mb-3" >
+        <br />
+        <div class="form-row mb-3">
           <div class="col-auto">
             <label>Cedula investigador</label>
           </div>
           <div class="col-auto">
-            <label for="inputCedulaInvestigador" class="visually-hidden">Cedula investigador</label>
-            <input v-model="idInvestigador" type="int" class="form-control" id="inputCedulaInvestigador" placeholder="Cedula" required>
-            <div class="invalid-feedback">Ingrese la cedula del investigador</div>
+            <label for="inputCedulaInvestigador" class="visually-hidden"
+              >Cedula investigador</label
+            >
+            <input
+              v-model="idInvestigador"
+              type="int"
+              class="form-control"
+              id="inputCedulaInvestigador"
+              placeholder="Cedula"
+              required
+            />
+            <div class="invalid-feedback">
+              Ingrese la cedula del investigador
+            </div>
           </div>
           <div class="col-auto">
-            <button type="submit" @click.prevent="agregarInvestigador" class="btn btn-outline-success mb-3" >Registrar en el espacio de trabajo</button>
+            <button
+              type="submit"
+              @click.prevent="agregarInvestigador"
+              class="btn btn-outline-success mb-3"
+            >
+              Registrar en el espacio de trabajo
+            </button>
+
+            <button
+              type="submit"
+              @click.prevent="modificarInvestigador"
+              class="btn btn-outline-success"
+              v-if="enEdicion"
+            >
+              Actualizar
+            </button>
           </div>
         </div>
       </form>
-      
-
 
       <section class="data">
-        <caption >
+        <caption>
           Integrantes
         </caption>
-          <table class="table">
-            <thead>
-              <tr>
-                <th>Codigo integrante</th>
-                <th>Cedula integrante</th>
-                <th>Rol</th>
-                <th>Acciones</th>
-                
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="integrante in integrantes" 
-              :key="integrante.idreseacher">
-                <td>{{ integrante.idresearcher_workspace }}</td>
-                <td>{{ integrante.researcher_document }}</td>
-
-                <td>
-                  <select v-model="rolInvestigador"  class="form-select"  aria-label="selectRol" id="selectRol" required>
-                    <option value="0">Sin rol</option>
-                    <option value="1">Co investigador</option>
-                    <option value="2">Auxiliar de investigacion</option>
-                    <option value="3">Investigador asociado</option>
-                  </select> <label> </label>
-                </td>
-                <td><button size="sm" @click.prevent="cargarIntegrante" class="btn btn-outline-primary" id="btn-mod">Modificar rol</button></td>
-              </tr>
-            </tbody>
-          </table>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Codigo integrante</th>
+              <th>Cedula integrante</th>
+              <th>Espacio Trabajo</th>
+              <th>Rol</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="integrante in integrantes" :key="integrante.idreseacher">
+              <td>{{ integrante.idresearcher_workspace }}</td>
+              <td>{{ integrante.researcher_document }}</td>
+              <td>{{ integrante.idworkspace }}</td>
+              <td
+                v-if="
+                  !enEdicion || idInvestigador != integrante.researcher_document
+                "
+              >
+                {{ integrante.idrole }}
+              </td>
+              <td
+                v-if="
+                  enEdicion && idInvestigador == integrante.researcher_document
+                "
+              >
+                <select
+                  v-model="rolInvestigador"
+                  class="form-select"
+                  aria-label="selectRol"
+                  id="selectRol"
+                  required
+                >
+                  <option value="0">Sin rol</option>
+                  <option value="1">Co investigador</option>
+                  <option value="2">Auxiliar de investigacion</option>
+                  <option value="3">Investigador asociado</option>
+                </select>
+                <label> </label>
+              </td>
+              <td>
+                <button
+                  size="sm"
+                  @click.prevent="cargarIntegrante"
+                  class="btn btn-outline-primary"
+                  id="btn-mod"
+                >
+                  Modificar rol
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </section>
-      
     </div>
   </div>
 </template>
@@ -83,13 +144,15 @@ export default {
     HelloWorld,
   },
   data: () => ({
-    idEspacioDeTrabajo: '',
+    idEspacioDeTrabajo: "",
     idWorkValido: true,
     espaciosTrabajo: [],
-    idInvestigador: '',
+    idInvestigador: "",
     idInvValido: true,
     investigadores: [],
-    integrantes: []
+    integrantes: [],
+    rolInvestigador: "",
+    enEdicion: false,
   }),
   created: function () {
     this.obtenerEspaciosDeTrabajo();
@@ -97,72 +160,61 @@ export default {
   },
   methods: {
     obtenerEspaciosDeTrabajo: function () {
-      axios.get("http://localhost:3001/workspace")
-        .then((result) => {
-          this.espaciosTrabajo = result.data.info
-          for (let i = 0; i < this.espaciosTrabajo.length; i++) {
-            console.log(this.espaciosTrabajo[i]['idworkspace'])
-            var html = this.stringToHTML(`<option value=${this.espaciosTrabajo[i]['idworkspace']}>${this.espaciosTrabajo[i]['workspace_name']}</option>`);
-            document.getElementById('selectIdEspacioTrabajo').appendChild(html)
-          }
-        });
+      axios.get("http://localhost:3001/workspace").then((result) => {
+        this.espaciosTrabajo = result.data.info;
+        for (let i = 0; i < this.espaciosTrabajo.length; i++) {
+          var html = this.stringToHTML(
+            `<option value=${this.espaciosTrabajo[i]["idworkspace"]}>${this.espaciosTrabajo[i]["workspace_name"]}</option>`
+          );
+          document.getElementById("selectIdEspacioTrabajo").appendChild(html);
+        }
+      });
     },
-    stringToHTML: function(s){
+    stringToHTML: function (s) {
       const parser = new DOMParser();
       const doc = parser.parseFromString(s, "text/html");
       return doc.body.firstChild;
     },
 
     obtenerIntegrantesEspacio: function () {
-      axios.get("http://localhost:3001/integrante")
-        .then((result) => {
-          this.integrantes = result.data.info.map(x => {
-            var f = Object.assign({}, x);
-            f.due_date = x.due_date.slice(0, 10);
-            return f;
-          });
-          for (let i = 0; i < this.integrantes.length; i++) {
-            const aux = this.integrantes[i];
-            switch (aux["status"]) {
-              case 0:
-                this.integrantes[i]["status"] = "No iniciado";
-                break;
-              case 1:
-                this.integrantes[i]["status"] = "En Curso";
-                break;
-              case 2:
-                this.integrantes[i]["status"] = "Finalizado";
-                break;
-              default:
-                break;
-            }
+      axios.get("http://localhost:3001/integrante").then((result) => {
+        this.integrantes = result.data.info;
+        for (let i = 0; i < this.integrantes.length; i++) {
+          const aux = this.integrantes[i];
+          switch (aux["idrole"]) {
+            case 0:
+              this.integrantes[i]["idrole"] = "Por Asignar Rol";
+              break;
+            case 1:
+              this.integrantes[i]["idrole"] = "Co-Investigador";
+              break;
+            case 2:
+              this.integrantes[i]["idrole"] = "Auxiliar-Investigador";
+              break;
+            case 3:
+              this.integrantes[i]["idrole"] = "investigador-Asociado";
+              break;
+            default:
+              break;
           }
-        });
+        }
+      });
     },
-    
-    /** 
-     obtenerInvestigadores: function () {
-      axios
-        .get("http://localhost:3001/investigador")
-        .then((result) => {
-          this.investigadores = result.data;
-          for (let i = 0; i < this.investigadores.length; i++) {
-            var html = this.stringToHTML(`<option value=${this.investigadores[i]['researcher_document']}>${this.investigadores[i]['first_name']} ${this.investigadores[i]['last_name']}</option>`);
-            document.getElementById('selectIdInvestigador').appendChild(html)
-          }
-        });
-    },
-    */
-    cargarIntegrante: function(e){
-      this.enEdicion = true
-      var fila = e.target.parentNode.parentNode; //Se almacena en una variable a la fila que la contiene
-      document.getElementById("inputCedulaInvestigador").setAttribute("disabled", "")
-      document.getElementById("selectIdEspacioTrabajo").setAttribute("disabled", "")
 
-      this.researcher_document = fila.cells[1].textContent
-      this.idworkspace = fila.cells[2].textContent
+    cargarIntegrante: function (e) {
+      this.enEdicion = true;
+      var fila = e.target.parentNode.parentNode; //Se almacena en una variable a la fila que la contiene
+      document
+        .getElementById("inputCedulaInvestigador")
+        .setAttribute("disabled", "");
+      document
+        .getElementById("selectIdEspacioTrabajo")
+        .setAttribute("disabled", "");
+
+      this.idInvestigador = fila.cells[1].textContent;
+      this.idEspacioDeTrabajo = fila.cells[2].textContent;
     },
-     validarFormulario: function () {
+    validarFormulario: function () {
       if (!this.idEspacioDeTrabajo) {
         this.idWorkValido = false;
       }
@@ -170,27 +222,57 @@ export default {
         this.idInvValido = false;
       }
     },
-    limpiarCampos: function(){
-      this.idEspacioDeTrabajo = '';
-      this.idInvestigador = '';
+    limpiarCampos: function () {
+      this.idEspacioDeTrabajo = "";
+      this.idInvestigador = "";
     },
-    agregarInvestigador: function() {
+    agregarInvestigador: function () {
       this.validarFormulario();
-      let testPost ={
+      let testPost = {
         researcher_document: this.idInvestigador,
-        idworkspace: this.idEspacioDeTrabajo
+        idworkspace: this.idEspacioDeTrabajo,
       };
-      axios.post('http://localhost:3001/integrante', testPost).then((resp) => {
-        if (resp.data.name == 'error') {
+      axios.post("http://localhost:3001/integrante", testPost).then((resp) => {
+        if (resp.data.name == "error") {
           this.limpiarCampos();
-          alert('No se puede ingresar el integrante porque ya se encuentra registrado');
-        } else if(resp.data.ok) {
+          alert(
+            "No se puede ingresar el integrante porque ya se encuentra registrado"
+          );
+        } else if (resp.data.ok) {
           this.limpiarCampos();
-          alert('Integrante registrado');
+          alert("Integrante registrado");
         }
-      })
+      });
       this.obtenerIntegrantesEspacio();
-    }
-  }
+    },
+    modificarInvestigador: function () {
+      this.validarFormulario();
+      let testPut = {
+        idrole: this.rolInvestigador,
+        idworkspace: this.idEspacioDeTrabajo,
+        researcher_document: this.idInvestigador,
+      };
+      axios
+        .put(
+          `http://localhost:3001/integrante/${testPut.idrole}/${testPut.researcher_document}/${testPut.idworkspace}`,
+          testPut
+        )
+        .then((resp) => {
+          if (resp.data.name == "error") {
+            alert("No se puede actualizar el usuario");
+          }
+          this.limpiarCampos();
+          this.obtenerIntegrantesEspacio();
+          document
+            .getElementById("inputCedulaInvestigador")
+            .removeAttribute("disabled");
+          document
+            .getElementById("selectIdEspacioTrabajo")
+            .removeAttribute("disabled");
+        });
+      alert("Investigador Actualizado");
+      this.enEdicion = false;
+    },
+  },
 };
 </script>
